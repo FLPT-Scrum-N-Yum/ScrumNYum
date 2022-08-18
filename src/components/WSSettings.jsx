8da9fea4a95be96185e6f6a1b5da9d34c0b15c37
Thang Thai <thaithangt@gmail.com>
2022-08-18 00:39:34 -0700

    UI Overhaul

--- src/components/WSSettings.jsx
+++ src/components/WSSettings.jsx
@@ -22,12 +22,11 @@ function WSSettings({ workspaces, setWorkspaces }) {
       headers: {
         'Content-Type': 'application/json',
       },
-    })
+    });
 
     // Workspace should be clean and no stickies on the view
     // Automatically set their workspace as the one they just created or the one you choose
     // how do we know which workspace user is clicking on?
-
 
     // .then((res) => {
     //   // pull a new list of workspaces
@@ -41,29 +40,29 @@ function WSSettings({ workspaces, setWorkspaces }) {
     // send this to backend to post
   };
 
-  const onDelete = (event) => {
-    event.preventDefault();
-    const wsName = document.getElementById('ws-name').value;
-    // send this to backend to delete
-    //NOT SET UP W/ BACKEND
-    
-  };
+  // const onDelete = (event) => {
+  //   event.preventDefault();
+  //   const wsName = document.getElementById('ws-name').value;
+  //   // send this to backend to delete
+  //   //NOT SET UP W/ BACKEND
+  // };
 
   return (
     // User types in ws name -> clicks create ws -> ws added to their ws/teams
     // User types in ws name -> clicks delete ws -> ws removed from their ws/teams (and db?)
-    <div className='ws-settings-container'>
-      <form id='ws_settings'>
+    <div>
+      <form id='ws_settings' className='ws-add'>
         <input
           required
           placeholder='Workspace Name'
           type='text'
           id='ws-name'
-          className='ws-name-text-box'
           name='ws-name'
         ></input>
-        <button onClick={onCreate}>Create Workspace</button>
-        <button onClick={onDelete}>Delete Workspace</button>
+        <button className='ws-btn' onClick={onCreate}>
+          Create Workspace
+        </button>
+        {/* <button onClick={onDelete}>Delete Workspace</button> */}
       </form>
     </div>
   );
